8e28704de0cfdf936fbf1e5f3e932bcefdbba046
koladem <arisekolakolade@gmail.com>
2021-01-18 10:58:41 +0000

    try taper frames

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
 import cv2
 
-print(cv2.__version__)
+cap = cv2.VideoCapture('ryutricks.mp4')
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('ryuoutput.avi', fourcc, 0.5, (640, 1136))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if ret:
+        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        out.write(frame)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+cap.release()
+out.release()
+cv2.destroyAllWindows()
